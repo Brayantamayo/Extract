51a6d684c34ffdbf66ebf3064eab2d3b59eadb34
Brayantamayo <brayancastaneda718@gmail.com>
2025-08-12 16:25:28 -0500

    Add files via upload

---
+++ abstract.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal miPerro = new Perro();
+            miPerro.HacerSonido(); 
+        }
+        // Clase abstracta
+        abstract class Animal
+        {
+            public abstract void HacerSonido();
+        }
+
+        // Clase concreta
+        class Perro : Animal
+        {
+            public override void HacerSonido()
+            {
+                Console.WriteLine("Guau");
+            }
+        }
+
+    }
+}
